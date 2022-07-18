9909dc4b93764802c8049d0bb9b2106bd6de2a7c
Priya <kshetri.priya@gmail.com>
2022-07-18 12:37:34 -0500

    validate email format

--- project.py
+++ project.py
@@ -15,3 +15,9 @@ def file_to_be_sent():
     path_file = glob.iglob(r'C:/Users/kshet/Desktop/daily_progress/*')
     attachment = max(path_file, key=os.path.getctime)
     return attachment
+
+def validate_email(email):
+    if not re.match(r'[a-z0-9\.-]+@[a-z]+\.[a-z]+', email):
+        return 'please use correct email'
+        exit(1)
+    return True
